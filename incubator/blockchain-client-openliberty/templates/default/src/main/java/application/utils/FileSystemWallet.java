b06c3ad421cd0cdb2629cf8e5615bfbf18f83eac
Jorge D. Rodriguez <jorgedr@us.ibm.com>
2020-10-29 13:58:38 -0400

    Add suppor for connection profile file and fs wallet

--- incubator/blockchain-client-openliberty/templates/default/src/main/java/application/utils/FileSystemWallet.java
+++ incubator/blockchain-client-openliberty/templates/default/src/main/java/application/utils/FileSystemWallet.java
@@ -1,9 +1,8 @@
 package application.utils;
 
-import java.io.IOException;
-import java.nio.file.FileSystems;
-import java.nio.file.InvalidPathException;
+import java.net.URI;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.logging.Logger;
 
 import org.hyperledger.fabric.gateway.Wallet;
@@ -23,17 +22,17 @@ public class FileSystemWallet {
             LOGGER.severe("Wallet profile was not specified... cannot retrieve wallet.");
             throw new IdentityException("Wallet profile not found.");
         }
-        
+
         try {
             walletProfile = new JSONObject(walletProfileString);
         } catch (JSONException e) {
-            LOGGER.severe("Could not parse the wallet profile: "+e.toString());
+            LOGGER.severe("Could not parse the wallet profile: " + e.toString());
             throw new IdentityException("Error parsing wallet profile.", e);
         }
 
         try {
             JSONObject options = walletProfile.getJSONObject("options");
-            walletPath = options.getString("path"); 
+            walletPath = options.getString("path");
             if (walletPath == null || walletPath.isEmpty()) {
                 LOGGER.severe("No path to the file system wallet was provided. Cannot retrieve the wallet.");
                 throw new IdentityException("Invalid wallet path provided.");
@@ -44,15 +43,18 @@ public class FileSystemWallet {
         }
 
         try {
-            Path basePath = FileSystems.getDefault().getPath(walletPath);
+            LOGGER.info("Wallet path attribute: " + walletPath);
+            URI walletURI = FileSystemWallet.class.getClassLoader().getResource(walletPath).toURI();
+            LOGGER.info("Wallet Path URI: " + walletURI);
+            Path basePath = Paths.get(walletURI); 
             wallet = Wallet.createFileSystemWallet(basePath);
             if (wallet == null) {
-                LOGGER.severe("Could not retrieve the wallet from the path specified: "+walletPath);
+                LOGGER.severe("Could not retrieve the wallet from the path specified: " + walletPath);
                 throw new IdentityException("Invalid wallet provided.");
             }
-        } catch (IOException | InvalidPathException e) {
-            LOGGER.severe("Exception processing wallet path: "+e.toString());
-            throw new IdentityException("Exception processing wallet path.");
+        } catch (Exception e) {
+            LOGGER.severe("Exception processing wallet path: " + e.toString());
+            throw new IdentityException("Exception processing wallet path.", e);
         }
         return wallet;
     }
